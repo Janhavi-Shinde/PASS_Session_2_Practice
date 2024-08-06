1a57a59f8c2ae6a2b6b0af1f8bbd5fbd58df360e
Janhavi Shinde <janashinde394@gmail.com>
2024-08-06 23:44:53 +1000

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Congratulations on successfully cloning and running a file form GitHub! :D ");
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
